1bcd0cd28a6b5323d66cb74e0dd47affda85fe45
Guanghui Wang <wghglory89@gmail.com>
2017-05-23 16:15:46 -0700

    init color organizer project

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// bootstrap core. specific plugin like jumbotron is imported in needed component.
+// import './bootstrap/_core.scss';
+
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
