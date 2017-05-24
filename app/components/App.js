410160971a9afc3079b82adb608c296b5a5d59c1
Guanghui Wang <wghglory89@gmail.com>
2017-05-23 23:57:53 -0700

    Color Component life events

--- app/components/App.js
+++ app/components/App.js
@@ -9,7 +9,32 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            colors: []
+            colors: [
+                {
+                    "id": "8658c1d0-9eda-4a90-95e1-8001e8eb6036",
+                    "title": "Ocean Blue",
+                    "color": "#0070ff",
+                    "rating": 3
+                },
+                {
+                    "id": "f9005b4e-975e-433d-a646-79df172e1dbb",
+                    "title": "Tomato",
+                    "color": "#d10012",
+                    "rating": 2
+                },
+                {
+                    "id": "58d9caee-6ea6-4d7b-9984-65b145031979",
+                    "title": "Lawn",
+                    "color": "#67bf4f",
+                    "rating": 1
+                },
+                {
+                    "id": "a5685c39-6bdc-4727-9188-6c9a00bf7f95",
+                    "title": "Party Pink",
+                    "color": "#ff00f7",
+                    "rating": 5
+                }
+            ]
         };
         this.addColor = this.addColor.bind(this);
         this.rateColor = this.rateColor.bind(this);
--- app/index.js
+++ app/index.js
@@ -4,19 +4,19 @@ import ReactDOM from 'react-dom';
 // bootstrap core. specific plugin like jumbotron is imported in needed component.
 // import './bootstrap/_core.scss';
 
-// import App from './components/App';
+import App from './components/App';
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('app')
-// );
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
 
 
 /* Clock demo */
-import Clock from './components/Clock';
-const target = document.getElementById('app');
+// import Clock from './components/Clock';
+// const target = document.getElementById('app');
 
-ReactDOM.render(
-    <Clock onClose={() => ReactDOM.unmountComponentAtNode(target)} />,
-    target
-);
+// ReactDOM.render(
+//     <Clock onClose={() => ReactDOM.unmountComponentAtNode(target)} />,
+//     target
+// );
