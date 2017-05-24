ffae4df29f44f8935bd88857877e90bf81a97d29
Guanghui Wang <wghglory89@gmail.com>
2017-05-24 11:16:46 -0700

    componentWillReceiveProps: HiddenMessages component

---
+++ app/components/HiddenMessages.js
@@ -0,0 +1,95 @@
+/**
+ * 3 messages, display 1 message at a single time, the others mark 'x'. Every second message switches.
+ */
+import React from 'React';
+import PropTypes from 'prop-types';
+
+class HiddenMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hidden: (props.hide) ? props.hide : true
+        };
+
+        this.hide = this.hide.bind(this);
+        this.show = this.show.bind(this);
+    }
+
+    hide() {
+        const hidden = true;
+        this.setState({ hidden });
+    }
+
+    show() {
+        const hidden = false;
+        this.setState({ hidden });
+    }
+
+    // parent HiddenMessages state showing is changing every second, so the prop hide={(i !== showing)} changes,
+    // child HiddenMessage doesn't know this and won't update state unless using `componentWillReceiveProps`
+    componentWillReceiveProps(nextProps) {
+        this.setState({ hidden: nextProps.hide });
+    }
+
+    render() {
+        const { children } = this.props;
+        const { hidden } = this.state;
+        return (
+            <p onMouseEnter={this.show}
+                onMouseLeave={this.hide}>
+                {(hidden) ?
+                    children.replace(/[a-zA-Z0-9]/g, "x") :
+                    children
+                }
+            </p>
+        );
+    }
+
+}
+HiddenMessage.propTypes = {
+    hide: PropTypes.bool,
+    children: PropTypes.string.isRequired
+};
+
+export default class HiddenMessages extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [
+                "The crow crows after midnight",
+                "Bring a watch and dark clothes to the spot",
+                "Jericho Jericho Go"
+            ],
+            showing: -1
+        };
+    }
+
+    // componentDidMount ok as well
+    componentWillMount() {
+        this.interval = setInterval(() => {
+            let { showing, messages } = this.state;
+            showing = (++showing >= messages.length) ?
+                -1 :
+                showing;
+            this.setState({ showing });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        const { messages, showing } = this.state;
+        return (
+            <div className="hidden-messages">
+                {messages.map((message, i) =>
+                    <HiddenMessage key={i}
+                        hide={(i !== showing)}>
+                        {message}
+                    </HiddenMessage>
+                )}
+            </div>
+        );
+    }
+}
--- app/index.js
+++ app/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom';
 // bootstrap core. specific plugin like jumbotron is imported in needed component.
 // import './bootstrap/_core.scss';
 
-import App from './components/App';
+// import App from './components/App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('app')
+// );
 
 
-/* Clock demo */
+/* Clock demo -- componentDidMount, componentWillUnmount, unmountComponentAtNode */
 // import Clock from './components/Clock';
 // const target = document.getElementById('app');
 
@@ -20,3 +20,10 @@ ReactDOM.render(
 //     <Clock onClose={() => ReactDOM.unmountComponentAtNode(target)} />,
 //     target
 // );
+
+/* Hidden Message demo -- componentWillReceiveProps */
+import HiddenMessages from './components/HiddenMessages';
+ReactDOM.render(
+    <HiddenMessages />,
+    document.getElementById('app')
+);
